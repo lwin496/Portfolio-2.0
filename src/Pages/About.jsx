3fab671f287ed020cc5169a4905732fe0b0fad2f
Louis Win <88466438+lwin496@users.noreply.github.com>
2022-11-30 22:04:36 -0700

    changed about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -11,30 +11,45 @@ const About = () => {
   <>
     <div className='outer-container overflow-hidden'>
       <Navbar pageWrapId={"page-wrap"} OuterContainerId={"outer-container"} />
-      <section className='page-wrap bg-[#301934] h-[100vh]  bg-no-repeat bg-cover' style={{backgroundImage:`url('https://images.hdqwalls.com/download/moon-fox-galaxy-4k-79-2560x1440.jpg')` }}>
+      <section className='page-wrap bg-[#301934] h-[100vh]  bg-no-repeat bg-cover' style={{backgroundImage:`url('https://res.cloudinary.com/duc4ngc5i/image/upload/v1669160739/Portfolio%202.0%20/orion-nebula-11107_f1zdoq.jpg')` }}>
             <div className='flex text-white flex-col'>
-                <h1 className='text-5xl m-auto mt-[10vh] mb-[1rem] font-bold'>ABOUT <span className='text-purple-400'>ME</span></h1>
-                <h3 className='m-auto mb-[1rem] w-[50%] font-semibold'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus animi eum, cumque expedita veritatis placeat neque nostrum et quae illo tempora voluptatem, totam magni non voluptates quasi aliquid ab, nemo aut tempore. T empore pariatur consequuntur dolorem nulla exercitationem quaerat consequatur.
-                </h3>
+                <h1 className='text-7xl m-auto mt-[5vh] mb-[0rem] font-bold'>ABOUT <span className='text-purple-400'>ME</span></h1>    
+                <div className='w-[25%] h-[.5rem] bg-white m-auto'></div>  
+                <div className='flex flex-col'>
+            </div>
+            <div className='grid grid-cols-2'>
+             <img src={Louis} alt="louis" className='m-auto' />
+             <div className='bg-gray-800 rounded-xl m-[2rem] bg-opacity-80  '>
+             <div className='w-[90%] m-auto pt-10'>
+                <h1 className='text-5xl font-bold pb-5'>Hello!</h1> 
+                <h3 className='text-xl'>Once Again, my name is Louis Win. I'm a West-Mec-Nec Year 2 Coding student, Also I am known to be accentric and quite lively. My strengths are my communications skills along with my knowlege about business and technology. I am currently the Vice President of my FBLA chapter 2022-2023 and was the Treasurer for the last year 2021-2022  </h3>   
                 
-                <div className='w-[55%] h-[.25rem] bg-white m-auto'></div>
-                  <div className='w-[10%] h-[10%]'>
-                    <a href="#" className='relative left-[55vw] top-[0vh]'>
-                      <img src={githubIcon} alt="mygithub" className='planet' />
-                    </a>
-                    <a href="#" className='relative left-[65vw] top-[10vh]'>
-                      <img src={linkedinIcon} alt="mylinkedIn" className='planet' />
-                    </a>
-                    <a href="#" className='relative left-[30vw] bottom-[20vh]'>
-                      <img src={instagramIcon} alt="myInstagram" className='planet' />
-                    </a>
-                    <a href="#" className='relative left-[30vw] bottom-[10vh]'>
-                      <img src={youtubeIcon} alt="myYoutubeicon" className='planet' />
-                    </a>
-                  </div>
-                <img src={Louis} alt="louis" className='h-[60%] left-[40%] fixed bottom-[0px]' />
-               
+                <div>
+
+                </div>
+   
+                <div className='flex h-[50%] w-[50%]'>
+                  <a href="#">
+                    <img src={githubIcon} alt="githublink" />
+                  </a>
+                  <a href="#">
+                    <img src={linkedinIcon} alt="linkedinicon" />
+                  </a>
+                  <a href="#">
+                    <img src={instagramIcon} alt="instagramicon" />
+                  </a>
+                  <a href="#">
+                    <img src={youtubeIcon} alt="youtubeicon" />
+                  </a>
+             </div>
+              
+
+                </div> 
+              
+             </div>
+            </div>
+             
+      
             </div>
       </section>
     </div>
@@ -43,3 +58,20 @@ const About = () => {
 }
 
 export default About
+
+
+{/* 
+                  <div className='w-[10rem] h-[10rem]'>
+                    <a href="#" className=''>
+                      <img src={githubIcon} alt="mygithub" className='' />
+                    </a>
+                    <a href="#" className=''>
+                      <img src={linkedinIcon} alt="mylinkedIn" className='' />
+                    </a>
+                    <a href="#" className=''>
+                      <img src={instagramIcon} alt="myInstagram" className='' />
+                    </a>
+                    <a href="#" className=''>
+                      <img src={youtubeIcon} alt="myYoutubeicon" className='' />
+                    </a>
+                  </div>  */}
