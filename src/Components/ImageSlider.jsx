602eb9d8c489eb027c2a98f2d5a9d6450b16e614
unknown <louiskyawwin1@gmail.com>
2022-12-02 04:58:27 -0700

    changed gallery

---
+++ src/Components/ImageSlider.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
+import Projects from '../Projects'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
+
+const ImageSlider = ({ slides }) => {
+
+      const [current, setCurrent] = useState(0)
+      const length = slides.length
+      
+      const nextSlide = () => { 
+            setCurrent(current === length - 1 ? 0 : current + 1)
+      }
+
+      const prevSlide = () => { 
+            setCurrent(current === 0 ? length - 1 : current - 1)
+      }
+
+      console.log(current)
+
+      if(!Array.isArray(slides) || slides.length <= 0){ 
+            return null 
+      }
+
+return (
+      <div className=''>
+            <FaArrowAltCircleLeft className='absolute top-[50%] left-[32px] text-5xl text-white cursor-pointer z-30 select-none' onClick={prevSlide}/>
+            <FaArrowAltCircleRight className='absolute top-[50%] right-[32px] text-5xl text-white cursor-pointer z-30 select-none' onClick={nextSlide}/>
+            {Projects.map((slide, index) => { 
+                  return( 
+                        <div className={index === current ? 'slide active' : 'slide'} key={slide.id}>
+                              {index === current && ( 
+                              <div className='text-white'> 
+                                    <img src={slide.image} alt={slide.title} className='opacity-50 h-[100vh] w-[100vw]' />
+                                    <h3 className='absolute text-7xl font-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-[-2rem]'>{slide.title}</h3>
+                                    <h3 className='absolute text-2xl semi-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-[3rem] text-center'>{slide.description}</h3>
+                                    <a href={slide.link} className='absolute text-2xl semi-bold top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mt-[8rem] bg-purple-800 p-2 font-semibold hover:bg-green-500'>Go to Page</a>
+                              </div>
+                             
+                              )}
+                              
+                        </div>
+                  )
+                 
+            })}
+      </div>
+  )
+}
+
+export default ImageSlider
--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Projects from '../Projects'
+import ImageSlider from '../Components/ImageSlider'
+
 const Gallery = () => {
   return (
     <>
       <div className='outer-container'>
         <Navbar pageWrapId={"page-wrap"} OuterContainerId={"outer-container"} />
-        <section className='page-wrap bg-[#301934] h-[100%]'>
+        <section className='page-wrap bg-[#301934] h-[100vh]'>
           {/* <img src="https://images.hdqwalls.com/download/moon-fox-galaxy-4k-79-2560x1440.jpg" alt="background" className='fixed z-1' /> */}
-          <h1 className='text-white text-center font-bold text-8xl py-[1%]'>MY PROJECTS</h1>
-          <div className='bg-white h-[.5rem] w-[50%] m-auto mb-[1%]'></div>
-          <div className='flex justify-center align-middle text-white'>
-              {/* <h1>Gallery</h1> */}
-              <ul>
+          {/* <h1 className='text-white text-center font-bold text-8xl py-[1%]'>MY PROJECTS</h1> */}
+          {/* <div className='bg-white h-[.5rem] w-[50%] m-auto mb-[1%]'></div> */}
+          {/* <div className='flex justify-center align-middle text-white'> */}
+            <ImageSlider slides={Projects} />
+          {/* </div> */}
+        </section>
+      </div>
+    </>
+    
+  )
+}
+
+
+
+
+
+export default Gallery 
+
+
+    {/* <h1>Gallery</h1> */}
+              {/* <ul>
                 {Projects.map((project) => { 
                   return ( 
                     <li key={project.id} className='w-[100%] mb-5  m-auto'>
@@ -26,17 +44,4 @@ const Gallery = () => {
                     </li>
                   )
                 })}
-              </ul>
-          </div>
-        </section>
-      </div>
-    </>
-    
-  )
-}
-
-
-
-
-
-export default Gallery 
+              </ul> */}
